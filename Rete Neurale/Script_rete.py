b93549da2cfd1cd4eaacb64bdcce77eb490806fd
Giovanni Spina <giovannispina123456@gmail.com>
2019-08-25 18:03:46 +0200

    binary conversion
    inizio conversione dati in binario

--- Rete Neurale/Script_rete.py
+++ Rete Neurale/Script_rete.py
@@ -14,6 +14,65 @@ PATH_DATASET_METRO='../Dataset/Acquisizioni/Metro/Conversione/'
 #righe per leggere il file csv
 
 #print(os.path.isfile(PATH_DATASET_AUTO+'Acquisizione{}.csv'.format(j)))
+class CharacterTable(object):
+    """Given a set of characters:
+    + Encode them to a one-hot integer representation
+    + Decode the one-hot or integer representation to their character output
+    + Decode a vector of probabilities to their character output
+    """
+    def __init__(self, chars):
+        """Initialize character table.
+
+        # Arguments
+            chars: Characters that can appear in the input.
+        """
+        print(set(chars))
+        self.chars = sorted(set(chars))
+        print(chars)
+        self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
+        self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
+    def encode(self, C, num_rows):
+        """One-hot encode given string C.
+        # Arguments
+            C: string, to be encoded.
+            num_rows: Number of rows in the returned one-hot encoding. This is
+                used to keep the # of rows for each data the same.
+        """
+        #				5 		11
+        x = np.zeros((num_rows*len(self.chars)))
+        '''
+        0 8
+        1 0
+        2 4
+        '''
+       	print(C)
+        
+       	for i, c in enumerate(C):
+        	print("i {}  c {} in {}".format(i,c, enumerate(C)))
+            	x[i*len(self.chars)+self.char_indices[c]] = 1
+
+        
+
+        '''
+         1 2 3 4 5 6 7 8 9 0 +
+       0 0 0 0 0 0 0 0 1 0 0 0 
+       1 0 0 0 0 0 0 0 0 0 1 0 
+       2 0 0 0 1 0 0 0 0 0 0 0 
+
+        '''
+        return x
+
+    def decode(self, x, calc_argmax=True):
+        """Decode the given vector or 2D array to their character output.
+        # Arguments
+            x: A vector or a 2D array of probabilities or one-hot representations;
+                or a vector of character indices (used with `calc_argmax=False`).
+            calc_argmax: Whether to find the character index with maximum
+                probability, defaults to `True`.
+        """
+        if calc_argmax:
+            x = x.argmax(axis=-1)
+        return ''.join(self.indices_char[x] for x in x)
 def getMatrix(path_dir):
 	j=1
 	x=[]
@@ -56,10 +115,29 @@ bici=getMatrix(PATH_DATASET_BICI)
 '''
 print(auto)
 print(bus)
-print(metro)
+print(metro) 
 print(train)
 print(walk)
 print(bici)
 '''
+chars = '0123456789-'
+ctable = CharacterTable(chars)
+print("numero di righe {} colonne {}". format(len(auto),len(auto[0])))
+
+x = np.zeros((len(auto), len(auto[0]), 5*len(chars)), dtype=np.bool)
+for i in range(0, len(auto)):
+	for j in range(0, len(auto[0])):
+		x[i,j]= ctable.encode(str(auto[i,j]),5)
+		
+		print(x[i,j])
+
+
+'''
+y = np.zeros((len(questions), DIGITS + 1, len(chars)), dtype=np.bool)
+for i, sentence in enumerate(questions):
+    x[i] = ctable.encode(sentence, MAXLEN)
+for i, sentence in enumerate(expected):
+    y[i] = ctable.encode(sentence, DIGITS + 1)
 matrix=np.concatenate((auto,bus,bici,train,metro,walk));
+'''
 print(matrix);
